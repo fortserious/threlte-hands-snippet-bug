88cc008f91f93911e3ca8fda8439cd056112f318
ross doran <fortserious@gmail.com>
2025-09-04 11:51:51 -0400

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+  import { Canvas } from '@threlte/core'
+  import { VRButton } from '@threlte/xr'
+  import Scene from './Scene.svelte'
+</script>
+<Canvas>
+  <Scene />
+</Canvas>
+<VRButton />
---
+++ src/routes/Scene.svelte
@@ -0,0 +1,41 @@
+<script>
+  import { T } from '@threlte/core';
+  import { XR, Controller, Hand, useXR } from '@threlte/xr'
+
+  const { isHandTracking } = useXR()
+
+</script>
+<T.PerspectiveCamera
+  makeDefault
+  position={[10, 10, 10]}
+  oncreate={(ref) => {
+    ref.lookAt(0, 1, 0)
+  }}
+/>
+
+<T.DirectionalLight position={[0, 10, 10]} />
+
+<XR />
+
+{#if $isHandTracking}
+
+<Hand left >
+      <T.Mesh>
+    <T.IcosahedronGeometry args={[0.2]} />
+    <T.MeshStandardMaterial color="turquoise" />
+  </T.Mesh>
+</Hand>
+
+<Hand right>
+      <T.Mesh>
+    <T.IcosahedronGeometry args={[0.2]} />
+    <T.MeshStandardMaterial color="pink" />
+  </T.Mesh>
+</Hand>
+
+{:else}
+
+<Controller left />
+<Controller right />
+{/if}
+
